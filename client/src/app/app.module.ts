138565267130736509c9ed4187b715aa9f52fce8
tobeking01 <78264849+tobeking01@users.noreply.github.com>
2023-09-25 15:15:51 -0500

    import: materials, tailwinds, flex-layout, highcharts. layout: default(sidebar), fullwidth.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { DefaultComponent } from './layouts/default/default.component';
+import { FullwidthComponent } from './layouts/fullwidth/fullwidth.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, DefaultComponent, FullwidthComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ client/src/app/layouts/fullwidth/fullwidth.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fullwidth',
+  templateUrl: './fullwidth.component.html',
+  styleUrls: ['./fullwidth.component.scss']
+})
+export class FullwidthComponent {
+
+}
