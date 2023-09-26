b6cfec5d45601023ff917604c9eb0beefa989c45
tobeking01 <78264849+tobeking01@users.noreply.github.com>
2023-09-26 13:40:42 -0500

    updated sidebar nav

---
+++ client/src/app/modules/manage-project/app-routing.module.ts
@@ -0,0 +1,71 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { DefaultComponent } from './layouts/default/default.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { FullwidthComponent } from './layouts/fullwidth/fullwidth.component';
+import { HomeComponent } from './modules/home/home.component';
+import { LoginComponent } from './modules/login/login.component';
+import { RegisterComponent } from './modules/register/register.component';
+import { ManageProjectComponent } from './modules/manage-project/manage-project.component';
+import { ManageTicketComponent } from './modules/manage-ticket/manage-ticket.component';
+import { TeamComponent } from './modules/team/team.component';
+import { ProfileComponent } from './modules/profile/profile.component';
+import { Routes } from '@angular/router';
+import { ForgetPasswordComponent } from './modules/forget-password/forget-password.component';
+
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DefaultComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+      {
+        path: 'manage-project',
+        component: ManageProjectComponent,
+      },
+      {
+        path: 'manage-ticket',
+        component: ManageTicketComponent,
+      },
+      {
+        path: 'team',
+        component: TeamComponent,
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent,
+      },
+    ],
+  },
+  {
+    path: '',
+    component: FullwidthComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+      {
+        path: 'forget-password',
+        component: ForgetPasswordComponent,
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
