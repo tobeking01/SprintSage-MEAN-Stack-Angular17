1b028c3b9a6cf40d8d8a9fc57c96f05aad67219b
tobeking01 <78264849+tobeking01@users.noreply.github.com>
2023-09-26 14:33:22 -0500

    reset component created

--- client/src/app/modules/login/login.component.ts
+++ client/src/app/modules/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar'; // <-- import MatSnackBar
 
 @Component({
   selector: 'app-login',
@@ -14,7 +15,8 @@ export class LoginComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
@@ -27,11 +29,18 @@ export class LoginComponent implements OnInit {
   login(): void {
     this.authService.loginService(this.loginForm.value).subscribe({
       next: (res) => {
-        alert('Login is Success!');
+        this.snackBar.open('Login is Successful!', 'Close', {
+          // <-- use MatSnackBar here
+          duration: 2000,
+        });
         this.loginForm.reset();
         this.router.navigate(['dashboard']);
       },
       error: (err) => {
+        this.snackBar.open('Login Failed! Please try again.', 'Close', {
+          // <-- and here
+          duration: 2000,
+        });
         console.log(err);
       },
     });
